fd3272c1a08044e3499c6b3bc01caf5348b0be05
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 18:08:29 +0100

    test

--- jokeservice/app.js
+++ jokeservice/app.js
@@ -19,7 +19,7 @@ let allowCrossDomain = function(req, res, next) {
 
 
 app.use(allowCrossDomain);
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 app.use(express.json());
 app.use('/joke', require('./routes/joke'));
 
