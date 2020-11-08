af2c28c09b8c53fe4ba259c994b43e2a28337102
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 15:16:41 +0100

    xd

--- jokeservice/app.js
+++ jokeservice/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use('/joke', require('./routes/joke'));
