ae7735153b768b72142403ec0a343b47390fff06
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 17:11:23 +0100

    dfss

--- jokeservice/app.js
+++ jokeservice/app.js
@@ -3,10 +3,25 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use('/joke', require('./routes/joke'));
-
 
 const port = process.env.PORT || config.localPort; // Heroku
 app.listen(port);
