7e9b0b5f2fffff755423317bffefc70cf3ad3c61
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 18:24:10 +0100

    xd

--- jokeservice/app.js
+++ jokeservice/app.js
@@ -3,7 +3,19 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-let allowCrossDomain = function(req, res, next) {
+app.use(allowCrossDomain);
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use('/joke', require('./routes/joke'));
+
+const port = process.env.PORT || config.localPort; // Heroku
+app.listen(port);
+console.log('Listening on port ' + port + ' ...');
+
+module.exports = app; // test
+
+
+var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -16,19 +28,4 @@ let allowCrossDomain = function(req, res, next) {
       next();
     }
 };
-
-
-app.use(allowCrossDomain);
-app.use(express.static(__dirname + '/'));
-app.use(express.json());
-app.use('/joke', require('./routes/joke'));
-
-const port = process.env.PORT || config.localPort; // Heroku
-app.listen(port);
-console.log('Listening on port ' + port + ' ...');
-
-module.exports = app; // test
-
-
-
 
