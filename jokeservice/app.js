46540d791e977445548ed585d0904e9e4e4005d1
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 18:16:26 +0100

    l

--- jokeservice/app.js
+++ jokeservice/app.js
@@ -3,23 +3,7 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-
-app.use(allowCrossDomain);
-app.use(express.static(__dirname + '/'));
+app.use(express.static(public));
 app.use(express.json());
 app.use('/joke', require('./routes/joke'));
 
