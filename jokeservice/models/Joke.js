0cf1826d60ac4b8126053fb01dd4912182956fcf
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 13:08:17 +0100

    hrtr

--- jokeservice/models/Joke.js
+++ jokeservice/models/Joke.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 
 const joke = new mongoose.Schema({
     setup: String,
-    punchline: String,
-    type: mongoose.ObjectId, ref: 'Joke' 
+    punchline: String
 });
 
 module.exports = mongoose.model('Joke', joke);
