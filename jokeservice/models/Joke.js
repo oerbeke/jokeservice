e885fd54aea15db6c13e1165e879a33b721a3528
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 12:58:38 +0100

    bhjklæ

--- jokeservice/models/Joke.js
+++ jokeservice/models/Joke.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 const joke = new mongoose.Schema({
     setup: String,
-    punchline: String
+    punchline: String,
+    type: mongoose.ObjectId, ref: 'Joke' 
 });
 
 module.exports = mongoose.model('Joke', joke);
