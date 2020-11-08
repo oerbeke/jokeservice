dc11951fbc12890312f527c948b581fccdea982d
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 17:09:30 +0100

    fd

--- jokeservice/controllers/controller.js
+++ jokeservice/controllers/controller.js
@@ -13,9 +13,8 @@ exports.createJoke = function (setup, punchline) {
 };
 
 exports.getJoke = function (jokeId) {
-    return Joke.findById(jokeId).exec();
+    return Joke.findById("jokeId").exec();
 };
-
 
 exports.getJokes = function () {
     return Joke.find().populate('_id').exec();
