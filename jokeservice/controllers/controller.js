58891ccdc169e1571105280101aecb5e989c32ac
29536896 <aaronsoennichsen@gmail.com>
2020-11-08 13:14:46 +0100

    xdddd

--- jokeservice/controllers/controller.js
+++ jokeservice/controllers/controller.js
@@ -13,7 +13,7 @@ exports.createJoke = function (setup, punchline) {
 };
 
 exports.getJoke = function (jokeId) {
-    return Joke.findById(jokeId).exec();
+    return Joke.findById("5fa5d56c868b5b1e08dd6d51").exec();
 };
 
 exports.getJokes = function () {
